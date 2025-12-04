22e20823ac89b97ea36f7b5045c90c310d1eb66c
yellowkappa <khh.pettersson@gmail.com>
2025-12-04 13:42:22 -0500

    send start command to git

---
+++ vm_init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install -y \
+    neovim \
+    python3 \
+    python3-pip \
+    python3-venv \
+    python3-dev \
+    gcc \
+    ripgrep \
+    fzf \
+    git \
+    curl \
+    wget \
+    unzip
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+fc-cache -fv
